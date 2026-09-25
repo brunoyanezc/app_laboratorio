b00631eaefb0b2d774f11486257d13fadbc5f034
Yanez <bruno.yanez@sgs.com>
2026-09-25 09:37:10 -0300

    quantitation debug if c23 null

--- src/calculations/quantification.py
+++ src/calculations/quantification.py
@@ -5,6 +5,13 @@ def calculate_wfamex(
     tcf,
     c23_tag_factor,
 ):
+
+    if area_c23 is None:
+        return 0.0
+
+    if area_c23 <= 0:
+        return 0.0
+
     return (
         area_x
         / area_c23
@@ -18,6 +25,10 @@ def calculate_wx(
     wfamex,
     ffax,
 ):
+
+    if wfamex is None:
+        return 0.0
+
     return wfamex * ffax
 
 
@@ -25,6 +36,13 @@ def calculate_g100g(
     wx,
     sample_weight_g,
 ):
+
+    if sample_weight_g is None:
+        return 0.0
+
+    if sample_weight_g <= 0:
+        return 0.0
+
     return (
         wx
         / sample_weight_g
